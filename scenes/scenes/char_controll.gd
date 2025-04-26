7fa1bff3fb98ff8a5372a871e440896444d3367c
Hirosam <samuel.pizzatto@gmail.com>
2025-04-26 00:20:13 -0300

    Sprite improvement and change mouse mode

--- scenes/scenes/char_controll.gd
+++ scenes/scenes/char_controll.gd
@@ -4,10 +4,13 @@ extends CharacterBody3D
 @export
 var speed := 2.0
 
-var angular_speed := 0.2
+@export
+var mouse_sensitivity := 0.25
+
 var inpt_mov_vec := Vector2()
 var inpt_rot := 0.0
 var mouse_mov := Vector2()
+var is_captured_mode := true
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -15,13 +18,20 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	inpt_mov_vec.x = Input.get_axis("player_mov_left","player_mov_right")
 	inpt_mov_vec.y = Input.get_axis("player_mov_up","player_mov_down")
+	if(Input.is_action_just_pressed("key_esc")):
+		is_captured_mode = !is_captured_mode
+		if(is_captured_mode):
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	inpt_mov_vec = inpt_mov_vec.normalized()
 
 func _physics_process(delta: float) -> void:
 	var velovity_2d := inpt_mov_vec * speed * delta
 	velovity_2d = velovity_2d.rotated(-rotation.y)
 	var velocity := Vector3(velovity_2d.x, 0.0, velovity_2d.y)
-	rotation.y += angular_speed*-mouse_mov.x*delta
+	if(is_captured_mode):
+		rotation.y += mouse_sensitivity*-mouse_mov.x*delta
 	mouse_mov = Vector2()
 	move_and_collide(velocity)
 	
