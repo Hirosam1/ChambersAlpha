7b3de25696c0ba4ebb57345a27a38a8087a65656
Hirosam <samuel.pizzatto@gmail.com>
2025-05-02 19:39:25 -0300

    Basic disable fire when character is dead

--- scripts/spell_spawner.gd
+++ scripts/spell_spawner.gd
@@ -9,9 +9,11 @@ var spell_asset : PackedScene
 
 @onready var cast_elapsed_time := delay
 
+var is_alive = true
+
 func _process(delta: float) -> void:
 	cast_elapsed_time += delta
-	if(Input.is_action_pressed("fire") and cast_elapsed_time >= delay):
+	if(is_alive and Input.is_action_pressed("fire") and cast_elapsed_time >= delay):
 		# Instantiate an asset defined on @export
 		var spell: Area3D = spell_asset.instantiate()
 		# Add the asset on the 'main' three (the current scene)
@@ -21,3 +23,9 @@ func _process(delta: float) -> void:
 		# We rotate a 'foward' vector with our rotation on the 'top' axis
 		spell.ready_spell(Vector3.FORWARD.rotated(Vector3.MODEL_TOP,global_rotation.y))
 		cast_elapsed_time = 0.0
+
+
+func _on_health_damaged(damage: float, current_health: float) -> void:
+	if(current_health <= 0):
+		is_alive = false
+		
