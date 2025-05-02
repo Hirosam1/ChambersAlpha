b9ec9f915568708f5cac99e7ab4f6ea2d73b2d46
Hirosam <samuel.pizzatto@gmail.com>
2025-05-02 20:07:52 -0300

    Disable hurtbox when slime is dead

--- scenes/assets/hurt_box.gd
+++ scenes/assets/hurt_box.gd
@@ -9,3 +9,9 @@ func _on_area_entered(area: Health) -> void:
 		var controller :EnemyAiController = get_parent_node_3d()
 		var velocity_knockback :=  controller.velocity.normalized() * knockback
 		area.hurt(damage, Vector2(velocity_knockback.x, velocity_knockback.z))
+
+
+func _on_health_damaged(damage: float, current_health: float) -> void:
+	if(current_health <= 0):
+		monitoring = false
+		monitorable = false
