ca3f1db084d8fb492bcf6518a77108ffb55765f8
Hirosam <samuel.pizzatto@gmail.com>
2025-05-02 01:21:15 -0300

    Added slime death animation

--- scripts/enemy_ai.gd
+++ scripts/enemy_ai.gd
@@ -2,12 +2,17 @@ class_name EnemyAiController
 extends CharacterBody3D
 
 @export var speed = 3.0
+@onready var animation_player := $AnimationPlayer
 
+var is_alive = true
 var current_target: Node3D
 var is_chasing := false
 
 func die() -> void:
-	queue_free()
+	if(is_alive):
+		is_alive = false
+		animation_player.play("slime_death")
+	#queue_free()
 
 func start_chase_target(target: Node) -> void:
 	current_target = target
@@ -18,7 +23,7 @@ func stop_chasing() -> void:
 	is_chasing = false
 	
 func _process(delta: float) -> void:
-	if(is_chasing):
+	if(is_chasing and is_alive):
 		var direction := (current_target.global_position - global_position).normalized()
 		velocity = direction*speed
 	else:
